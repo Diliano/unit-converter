ff019fa06058e8bece8af2589e88162b55412bb8
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2023-10-24 15:48:21 +0100

    Added converVolume() function that is invoked when user enters a value and clicks the convert button

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ inputEl.addEventListener("input", function() {
 
 const convertBtn = document.getElementById("convert-btn");
 const lengthResult = document.getElementById("length-result");
+const volumeResult = document.getElementById("volume-result");
 
 convertBtn.addEventListener("click", function() {
     convertLength();
+    convertVolume();
 });
 
 function convertLength() {
     const metersToFeet = (inputEl.value * 3.281).toFixed(3);
     const feetToMeters = (inputEl.value / 3.281).toFixed(3);
     lengthResult.textContent = `${inputEl.value} meters = ${metersToFeet} feet | ${inputEl.value} feet = ${feetToMeters} meters`;
+}
+
+function convertVolume() {
+    const litersToGallons = (inputEl.value * 0.264).toFixed(3);
+    const gallonsToLiters = (inputEl.value / 0.264).toFixed(3);
+    volumeResult.textContent = `${inputEl.value} liters = ${litersToGallons} gallons | ${inputEl.value} gallons = ${gallonsToLiters} liters`;
 }
